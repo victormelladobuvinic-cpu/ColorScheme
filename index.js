753ee7c07fab76b2714d64ad0c4541d6bc4dd9af
victormelladobuvinic-cpu <victor.melladobuvinic@gmail.com>
2026-01-30 15:20:44 -0300

    refaction DRYer DOM manipulation functions, using .map and .join("")

--- index.js
+++ index.js
@@ -33,22 +33,15 @@ function sendRequestFunction(color, mode) {
     .then(res => res.json())
     .then(data => {
         
-        const colors = data.colors.map(color => { return color.hex.value})
+        const colors = data.colors.map(color => color.hex.value)
         
-        const colorScheme =  `
-             <div class="color-column" style="background-color: ${colors[0]};"></div>
-             <div class="color-column" style="background-color: ${colors[1]};"></div>
-             <div class="color-column" style="background-color: ${colors[2]};"></div>
-             <div class="color-column" style="background-color: ${colors[3]};"></div>
-             <div class="color-column" style="background-color: ${colors[4]};"></div>
-             `
-        const colorName = `
-             <p class="color-name">${colors[0]}</p>
-             <p class="color-name">${colors[1]}</p>
-             <p class="color-name">${colors[2]}</p>
-             <p class="color-name">${colors[3]}</p>
-             <p class="color-name">${colors[4]}</p>
-             ` 
+        const colorScheme = colors
+            .map(color => `<div class="color-column" style="background-color: ${color};"></div>`)
+            .join('')
+        
+        const colorName = colors
+            .map(color => `<p class="color-name">${color}</p>`)
+            .join('')
 
        document.querySelector(".color-scheme").innerHTML = colorScheme 
         document.querySelector(".name-scheme").innerHTML = colorName 
